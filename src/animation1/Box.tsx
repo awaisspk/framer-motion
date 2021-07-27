788f7c0ed703f09c58d922a4edf7ab03ec1e6111
awaisspk <iamawaisiqball@gmail.com>
2021-07-27 10:27:11 +0500

    learned basics of framer-motion

--- src/animation1/Box.tsx
+++ src/animation1/Box.tsx
@@ -1,7 +1,22 @@
 import { motion } from "framer-motion";
 
 const Box = () => {
-  return <motion.div animate={{ scale: 2 }} />;
+  return (
+    <motion.div
+      animate={{
+        scale: [1, 1, 2, 2, 1],
+        rotate: [0, 0, 270, 270, 0],
+        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+      }}
+      transition={{
+        duration: 2,
+        ease: "easeInOut",
+        times: [0, 0.2, 0.5, 0.8, 1],
+        loop: Infinity,
+        repeatDelay: 1,
+      }}
+    />
+  );
 };
 
 export default Box;
--- src/animation1/Main.tsx
+++ src/animation1/Main.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import styles from "../animation1/styles.module.css";
 import Box from "./Box";
+import Refresh from "./Refresh";
 
 const Main = () => {
   const [count, setCount] = useState(0);
   return (
     <>
+      <Refresh onClick={() => setCount(count + 1)} />
       <div className={styles.Box}>
-        <Box />
+        <Box key={count} />
       </div>
     </>
   );
